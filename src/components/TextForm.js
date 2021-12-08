a168ec52d7aa4953837cf10781bcf31845ddff4c
Sumit kumar sahoo <sumitsahoof5@gmail.com>
2021-12-09 00:54:15 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,8 +34,8 @@ export default function TextForm(props) {
         console.log("I am copy");
         var text = document.getElementById("myBox");
         text.select();
-        text.setSelectionRange(0, 9999);
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success");
     }
 
@@ -50,21 +50,21 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             <div>Simply enter your text and choose the case you want to convert it to.</div>
             <div className="mb-3">
-                <textarea className="form-control" placeholder="Enter Text Here.." value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#343a40':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" placeholder="Enter Text Here.." value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#212529':'#f8f9fa', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpCLick}>UPPERCASE &gt;</button>
-            <button className="btn btn-primary mx-3" onClick={handleLoCLick}>lowercase &gt;</button>
-            <button className="btn btn-primary mx-1" onClick={handleClCLick}>Reset &gt;</button>
-            <button className="btn btn-primary mx-3" onClick={handleCaCLick}>Capitalize &gt;</button>
-            <button className="btn btn-primary mx-1" onClick={handleCoCLick}>Copy &gt;</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpCLick}>UPPERCASE &gt;</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoCLick}>lowercase &gt;</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClCLick}>Reset &gt;</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCaCLick}>Capitalize &gt;</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCoCLick}>Copy &gt;</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your words Summary:</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length}Minutes to read</p>
+            <p>{text.split(/\s/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}Minutes to read</p>
             <h3>Preview:</h3>
             <div className="mb-3">
-                <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#343a40':'white', color: props.mode==='dark'?'white':'black'}} value={text} rows="15"></textarea>
+                <textarea className="form-control" placeholder=". . ." style={{backgroundColor: props.mode==='dark'?'#212529':'#f8f9fa', color: props.mode==='dark'?'white':'black'}} value={text} rows="15"></textarea>
                 
             </div>
         </div>
